4c3cfdb4612012764530971e4b543241be479a2d
Yuta iwasaki <yanyong0127@gmail.com>
2022-02-13 17:38:39 +0900

    About Column

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,84 @@ class  extends StatelessWidget {
 }
 */
 //StatelessWidgetによってホットリードができるらしい
-class MyApp extends StatelessWidget {
+class  MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         //Containerは制約がないと大きくなりすぎる
         body: SafeArea(
-          child: Container(
-            margin: const EdgeInsets.only(left: 10),
-            padding: const EdgeInsets.all(10),
-            height: 100.0,
-            width: 100,
-            color: Colors.teal,
-            child: const Center(
-              child: Text(
-                'Container center',
-                textAlign: TextAlign.center,
+          child: Center(
+              //Columnを使うことによって子ウィジェットを縦に並べて表示する事ができる
+              child: Column(
+               //mainAxisAlignmentはchildrenの配置についてかな　MainAxisSize.minだと全く意味ない
+               //mainAxisAlignment: MainAxisAlignment.center,
+               //mainAxisAlignment: MainAxisAlignment.end,
+               //mainAxisAlignment: MainAxisAlignment.spaceAround,
+               //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               //mainAxisAlignment: MainAxisAlignment.start, //デフォ
+               //mainAxisSize: MainAxisSize.max,//column自体が小さくなる //デフォMainAxisSize.max
+              //verticalDirection: VerticalDirection.up,//VerticalDirection.upすると物理的に画面を逆転したみたいになる
+            //crossAxisAlignmentを使って交差する軸を揃える
+            crossAxisAlignment: CrossAxisAlignment.stretch,//stretchは中点で揃える
+
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                padding: const EdgeInsets.all(10),
+                height: 100.0,
+                width: 100,
+                color: const Color.fromARGB(255, 48, 117, 173),
+                child: const Center(
+                  child: Text(
+                    'Container0 center',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 187, 56, 47),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                padding: const EdgeInsets.all(10),
+                height: 100.0,
+                width: 100,
+                color: Colors.white,
+                child: const Center(
+                  child: Text(
+                    'Container1 center',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                padding: const EdgeInsets.all(10),
+                height: 100.0,
+                width: 100,
+                color: const Color.fromARGB(255, 186, 52, 43),
+                child: const Center(
+                  child: Text(
+                    'Container2 center',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 20,
+                color: Colors.yellow,
+              )
+            ],
+          )),
         ),
       ),
     );
