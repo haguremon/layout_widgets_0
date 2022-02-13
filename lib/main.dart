eac1031de9dfa33bf4ba6fcdaaa125afe9ee85b9
Yuta iwasaki <yanyong0127@gmail.com>
2022-02-13 18:06:35 +0900

    About Row

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class  extends StatelessWidget {
 }
 */
 //StatelessWidgetによってホットリードができるらしい
-class  MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,23 +28,22 @@ class  MyApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
               //Columnを使うことによって子ウィジェットを縦に並べて表示する事ができる
-              child: Column(
-               //mainAxisAlignmentはchildrenの配置についてかな　MainAxisSize.minだと全く意味ない
-               //mainAxisAlignment: MainAxisAlignment.center,
-               //mainAxisAlignment: MainAxisAlignment.end,
-               //mainAxisAlignment: MainAxisAlignment.spaceAround,
-               //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-               //mainAxisAlignment: MainAxisAlignment.start, //デフォ
-               //mainAxisSize: MainAxisSize.max,//column自体が小さくなる //デフォMainAxisSize.max
-              //verticalDirection: VerticalDirection.up,//VerticalDirection.upすると物理的に画面を逆転したみたいになる
+              child: Row(
+            //mainAxisAlignmentはchildrenの配置についてかな　MainAxisSize.minだと全く意味ない
+            // mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.end,
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //mainAxisAlignment: MainAxisAlignment.start, //デフォ
+            mainAxisSize:
+                MainAxisSize.min, //column自体が小さくなる //デフォMainAxisSize.max
+            //verticalDirection: VerticalDirection.up,//VerticalDirection.upすると物理的に画面を逆転したみたいになる
             //crossAxisAlignmentを使って交差する軸を揃える
-            crossAxisAlignment: CrossAxisAlignment.stretch,//stretchは中点で揃える
+            //crossAxisAlignment: CrossAxisAlignment.stretch,//stretchは中点で揃える
 
             children: [
               Container(
-                margin: const EdgeInsets.only(left: 10),
-                padding: const EdgeInsets.all(10),
-                height: 100.0,
+                height: 100,
                 width: 100,
                 color: const Color.fromARGB(255, 48, 117, 173),
                 child: const Center(
@@ -58,9 +57,7 @@ class  MyApp extends StatelessWidget {
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 10),
-                padding: const EdgeInsets.all(10),
-                height: 100.0,
+                height: 100,
                 width: 100,
                 color: Colors.white,
                 child: const Center(
@@ -74,9 +71,7 @@ class  MyApp extends StatelessWidget {
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(left: 10),
-                padding: const EdgeInsets.all(10),
-                height: 100.0,
+                height: 100,
                 width: 100,
                 color: const Color.fromARGB(255, 186, 52, 43),
                 child: const Center(
@@ -90,9 +85,18 @@ class  MyApp extends StatelessWidget {
                 ),
               ),
               Container(
-                width: double.infinity,
-                height: 20,
-                color: Colors.yellow,
+                height: 100,
+                width: 100,
+                color: Colors.white,
+                child: const Center(
+                  child: Text(
+                    'Container3 center',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
               )
             ],
           )),
