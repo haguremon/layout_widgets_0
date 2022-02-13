369c98defdae0fab11d2b4368211c9ac6debb9b6
Yuta iwasaki <yanyong0127@gmail.com>
2022-02-13 18:16:24 +0900

    Columnの中にRowを入れる形でやってみた

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,11 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
               //Columnを使うことによって子ウィジェットを縦に並べて表示する事ができる
-              child: Row(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                verticalDirection: VerticalDirection.up,
+                children: [
+                  Row(
             //mainAxisAlignmentはchildrenの配置についてかな　MainAxisSize.minだと全く意味ない
             // mainAxisAlignment: MainAxisAlignment.center,
             //mainAxisAlignment: MainAxisAlignment.end,
@@ -36,70 +40,144 @@ class MyApp extends StatelessWidget {
             //mainAxisAlignment: MainAxisAlignment.spaceBetween,
             //mainAxisAlignment: MainAxisAlignment.start, //デフォ
             mainAxisSize:
-                MainAxisSize.min, //column自体が小さくなる //デフォMainAxisSize.max
+                    MainAxisSize.min, //column自体が小さくなる //デフォMainAxisSize.max
             //verticalDirection: VerticalDirection.up,//VerticalDirection.upすると物理的に画面を逆転したみたいになる
             //crossAxisAlignmentを使って交差する軸を揃える
             //crossAxisAlignment: CrossAxisAlignment.stretch,//stretchは中点で揃える
 
             children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: const Color.fromARGB(255, 48, 117, 173),
-                child: const Center(
-                  child: Text(
-                    'Container0 center',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 187, 56, 47),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: const Color.fromARGB(255, 48, 117, 173),
+                    child: const Center(
+                      child: Text(
+                        'Column0 Container0 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 187, 56, 47),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: const Center(
-                  child: Text(
-                    'Container1 center',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black,
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'Column0 Container1 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: const Color.fromARGB(255, 186, 52, 43),
-                child: const Center(
-                  child: Text(
-                    'Container2 center',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.green,
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: const Color.fromARGB(255, 186, 52, 43),
+                    child: const Center(
+                      child: Text(
+                        'Column0 Container2 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.green,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.white,
-                child: const Center(
-                  child: Text(
-                    'Container3 center',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.black,
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'Column0 Container3 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              )
+                  )
             ],
-          )),
+          ),
+           Row(
+            //mainAxisAlignmentはchildrenの配置についてかな　MainAxisSize.minだと全く意味ない
+            // mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.end,
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //mainAxisAlignment: MainAxisAlignment.start, //デフォ
+            mainAxisSize:
+                    MainAxisSize.min, //column自体が小さくなる //デフォMainAxisSize.max
+            //verticalDirection: VerticalDirection.up,//VerticalDirection.upすると物理的に画面を逆転したみたいになる
+            //crossAxisAlignmentを使って交差する軸を揃える
+            //crossAxisAlignment: CrossAxisAlignment.stretch,//stretchは中点で揃える
+
+            children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'Column1 Container0 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.red,
+                    child: const Center(
+                      child: Text(
+                        'Column1 Container1 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color:  Colors.white,
+                    child: const Center(
+                      child: Text(
+                        'Column1 Container2 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue,
+                    child: const Center(
+                      child: Text(
+                        'Column1 Container3 center Text',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  )
+            ],
+          ),
+                ],
+              )),
         ),
       ),
     );
